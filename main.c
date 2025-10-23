3a3ea92f8179b38cee386b302270a9fcee21f983
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-22 22:29:53 -0300

    [chore]: LinkedList

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 
 int main(void) {
   LinkedList *list = init_list();
-  Node *node1 = create_node(2, 'P');
-  Node *node2 = create_node(3, 'P');
-  Node *node3 = create_node(4, 'C');
-  insert_at_head(list, node1);
-  insert_at_head(list, node2);
-  insert_at_head(list, node3);
+  Node *node1 = create_node(1, 'P');
+  Node *node2 = create_node(2, 'P');
+  Node *node3 = create_node(3, 'C');
+  insert_based_on_priority(list, node1);
+  insert_based_on_priority(list, node3);
+  insert_based_on_priority(list, node2);
   print_list(list);
   return 0;
 }
