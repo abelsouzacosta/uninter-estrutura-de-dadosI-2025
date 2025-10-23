3d3561e7ea2b90770995ee026ff32376755ea4f1
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-22 22:40:21 -0300

    [chore]: UI
    Adding headers for UI functions

---
+++ headers/ui.h
@@ -0,0 +1,16 @@
+#ifndef UI
+#define UI
+
+#include "priority_list.h"
+
+void inserirSemPrioridade(LinkedList *list);
+
+void inserirComPrioridade(LinkedList *list);
+
+void inserir(LinkedList *list);
+
+void imprimirFilaPacientes(LinkedList *list);
+
+void atenderPaciente(LinkedList *list);
+
+#endif
